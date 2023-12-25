43d84731475006376c957364bc206230957289d7
Abhishek <abhishekparmar162@gmail.com>
2023-12-25 17:00:18 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import connectDb from "./db/index.js";
 
 dotenv.config({ path: "./env" });
 
-connectDb();
+const port = process.env.PORT || 3000;
+
+connectDb()
+  .then(() =>
+    app.listen(port, () => {
+      console.log(`listening on ${port}`);
+    })
+  )
+  .catch((err) => {
+    console.log("Error in mongoDB", err);
+  });
